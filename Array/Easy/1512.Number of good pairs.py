cbc90598bda3acdc6603e5121f1d91961a92e76c
SutakarSj <sjsutakar@gmail.com>
2025-04-17 20:55:45 +0530

    Array problems'

---
+++ Array/Easy/1512.Number of good pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        good_pairs=[]
+        for i in range (len(nums)):
+            for j in range(len(nums)):
+                if nums[i]==nums[j] and i<j: 
+                    k = (i,j)
+                    good_pairs.append(k)
+                    continue 
+        return len(good_pairs)
+        
