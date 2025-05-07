17c96da57397ab08f913eded71cb1750b66dbc43
SutakarSj <sjsutakar@gmail.com>
2025-05-07 23:14:17 +0530

    binary search probs

---
+++ binary search/Easy/69.Sqrt(x).py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r=1,x
+        while l<=r:
+            mid=(l+r)//2
+            m=mid*mid
+            if m==x:
+                return mid
+            elif m<x:
+                l=mid+1
+            else:
+                r=mid-1
+        return r
