17e58c0610cd64d30b031f62ca8c6303a673fb88
SutakarSj <sjsutakar@gmail.com>
2025-05-09 18:41:43 +0530

    hash table probs

---
+++ Hash Table/Easy/1790.Check if One String Swap Can Make Strings Equal.py
@@ -0,0 +1,6 @@
+﻿class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1==s2:return True
+        mismatch=[(a,b) for a,b in zip(s1,s2) if a!=b]
+        if len(mismatch)==2 and mismatch[0]==mismatch[1][::-1]:return True
+        return False
