7d3c04b1ea1335051d01150a5c4c0f9c7f768ead
SutakarSj <sjsutakar@gmail.com>
2025-05-10 23:12:30 +0530

    hash table probs

---
+++ Hash Table/Easy/2418.Sort the People.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        about_heights = {} #Hash Table
+        for x,y in zip(heights,names):
+            about_heights[x] = y
+        ans = []
+        for i in reversed(sorted(heights)):
+            ans.append(about_heights[i])
+        return ans        
