0c14d6a4ab20415230846d9caccad5badcba44d9
SutakarSj <sjsutakar@gmail.com>
2025-05-11 23:32:36 +0530

    string probs

---
+++ String/Easy/392.Is Subsequence.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i,j=0,0
+        l=len(t)
+        r=len(s)
+        while(i<r and j<l):
+            if (s[i]==t[j]):
+                i+=1
+                j+=1
+            else:
+                j+=1
+        if i==r:
+            return True
+        else:
+            return False
