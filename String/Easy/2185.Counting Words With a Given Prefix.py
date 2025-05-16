03953a246045fe1ce30b9e012807ae39170c23be
SutakarSj <sjsutakar@gmail.com>
2025-05-16 19:06:41 +0530

    String probs

---
+++ String/Easy/2185.Counting Words With a Given Prefix.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+        word_len = len(words)
+        for i in range(word_len):
+            if words[i].startswith(pref):
+                count+=1
+        return count
