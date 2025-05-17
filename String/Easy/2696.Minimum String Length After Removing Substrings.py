924485e46ead95ca659c07fcf25a8ba62664ee90
SutakarSj <sjsutakar@gmail.com>
2025-05-17 23:51:32 +0530

    string probs

---
+++ String/Easy/2696.Minimum String Length After Removing Substrings.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def minLength(self, s: str) -> int:
+        u=len(s)
+        A,B="AB","CD"
+        for i in s:
+            if A in s:s=s.replace(A,'')
+            elif B in s:s=s.replace(B,'')
+        return len(s)
