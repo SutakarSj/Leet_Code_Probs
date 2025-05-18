2f08a7cf94aa54f0d0b37e548ba4e0398dd31f49
SutakarSj <sjsutakar@gmail.com>
2025-05-18 19:43:12 +0530

    string probs

---
+++ String/Easy/3042.Count Prefix and Suffix Pairs I.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        count = 0
+        for i in range(len(words)):
+            for j in range(i+1,len(words)):
+                str1,str2=words[i],words[j]
+                len_str1=len(str1)
+                if str1 == str2[:len_str1] and str1 == str2[-len_str1:]:
+                    count+=1
+        return count
