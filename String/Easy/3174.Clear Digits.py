6a25fe3648d9586f9685d4de600d46a8c19d2ddf
SutakarSj <sjsutakar@gmail.com>
2025-05-19 21:38:10 +0530

    string probs

---
+++ String/Easy/3174.Clear Digits.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def clearDigits(self, s: str) -> str:
+        ans=[]
+        for i in s:
+            if i.isdigit():
+                ans.pop()
+            else:
+                ans.append(i)
+        return ''.join(ans)
