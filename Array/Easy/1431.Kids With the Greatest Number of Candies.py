1060b71b8bcfd1f09c8f09b7eccd60834183451b
SutakarSj <sjsutakar@gmail.com>
2025-05-21 19:39:38 +0530

    array probs

---
+++ Array/Easy/1431.Kids With the Greatest Number of Candies.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        ans,n,m=[],len(candies),extraCandies
+        val=max(candies)
+        for i in range(n):
+            if (candies[i]+m)>=val:
+                ans.append(True)
+            else:
+                ans.append(False) 
+        return ans
