962d41b83bdf1e176fca979797384b588668f14a
SutakarSj <sjsutakar@gmail.com>
2025-05-23 21:29:42 +0530

    hash table probs

---
+++ Hash Table/Easy/1512.Number of Good Pairs(hash table).py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        good_pair=0
+        count=defaultdict(int)
+        for i in nums:
+            good_pair += count[i]
+            count[i] += 1
+        
+        return good_pair
