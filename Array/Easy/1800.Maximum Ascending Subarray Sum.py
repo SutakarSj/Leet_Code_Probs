bb58195243789c6f9caeb0fe232af5629b5b6875
SutakarSj <sjsutakar@gmail.com>
2025-05-24 23:19:31 +0530

    array probs

---
+++ Array/Easy/1800.Maximum Ascending Subarray Sum.py
@@ -0,0 +1,12 @@
+﻿class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        maxi=0
+        n=len(nums)
+        current=nums[0]
+        for i in range(1,n):
+            if nums[i-1]<nums[i]:
+                current+=nums[i]
+            else:
+                maxi=max(maxi,current)
+                current=nums[i]
+        return max(maxi,current)
