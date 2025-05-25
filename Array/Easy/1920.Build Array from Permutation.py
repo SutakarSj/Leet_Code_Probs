636c6a4bbf07a118f862fdece360fcf67e753a99
SutakarSj <sjsutakar@gmail.com>
2025-05-25 22:50:12 +0530

    array probs

---
+++ Array/Easy/1920.Build Array from Permutation.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans,i,n=[],0,len(nums)
+        while i<n:
+            a=nums[nums[i]]
+            ans.append(a)
+            i+=1
+        return ans
